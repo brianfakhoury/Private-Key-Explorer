a602dca258ff76f824b9bfe2c75080a3a02cea23
Brian Fakhoury <brianfakhoury@gmail.com>
2022-05-01 13:07:30 -0400

    push

--- pages/index.js
+++ pages/index.js
@@ -101,6 +101,8 @@ export default function Home() {
           content="Fun & simple tool for browsing private key to public ethereum address mappings."
           key="ogdesc"
         />
+        <meta name="viewport" content= "width=device-width, user-scalable=no">
+
       </Head>
 
       <main className={styles.main}>
