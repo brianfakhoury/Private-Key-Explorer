392e43fd1350ccac97757f4767711371973afc39
Brian Fakhoury <brianfakhoury@gmail.com>
2023-09-06 13:48:39 -0600

    opengraph updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,23 @@
 export const metadata = {
-  title: 'Explore Private Keys',
-  description: 'This tool is for playing around with the mapping of private keys to Ethereum addresses.',
-  keywords: ['ethereum', 'cryptography', 'private keys'],
-}
+  title: "Explore Private Keys",
+  description:
+    "This tool is for playing around with the mapping of private keys to Ethereum addresses.",
+  keywords: ["ethereum", "cryptography", "private keys"],
+  openGraph: {
+    title: "Explore Private Keys",
+    description:
+      "This tool is for playing around with the mapping of private keys to Ethereum addresses.",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
